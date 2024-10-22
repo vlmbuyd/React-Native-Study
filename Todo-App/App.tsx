f8474de7ae6d79269e2b60255abc8fde7f22eba4
Yean <kjan0929@naver.com>
2024-10-22 11:43:55 +0900

    chore: 구조 잡기

--- Todo-App/App.tsx
+++ Todo-App/App.tsx
@@ -1,13 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import MainScreen from "./screens/MainScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MainScreen />;
 }
 
 const styles = StyleSheet.create({
---
+++ Todo-App/screens/MainScreen.tsx
@@ -0,0 +1,57 @@
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={"default"} />
+      <Text style={styles.pageTitle}>ToDo App</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할일</Text>
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 20 : 0,
+    backgroundColor: "#f7f8fa",
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 54,
+    fontWeight: "600",
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: " rgba(0, 0, 0, 0.2)",
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: "500",
+  },
+});
