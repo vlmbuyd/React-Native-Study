7440d9338ba18ef4b1beeb3186991d206d4cc2e1
Yean <kjan0929@naver.com>
2024-11-21 21:55:48 +0900

    useSelector, useDispatch 이용한 간단한 투두 앱 구현

--- redux-prac/src/index.tsx
+++ redux-prac/src/index.tsx
@@ -4,25 +4,21 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 const store = createStore(rootReducer);
 
-store.dispatch({
-  type: "ADD_TODO",
-  text: "USE REDUX",
-});
-
-console.log("store.getState", store.getState());
-
 const render = () =>
   root.render(
     <React.StrictMode>
-      <App
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-      />
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
     </React.StrictMode>
   );
 
