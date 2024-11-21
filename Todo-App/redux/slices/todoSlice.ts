4b925ba205e1affca184741f83aaef2145736312
Yean <kjan0929@naver.com>
2024-11-21 23:12:52 +0900

    feat: redux slice 및 store 생성

---
+++ Todo-App/redux/slices/todoSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Todo {
+  id: number;
+  text: string;
+  state: string;
+}
+
+interface TodoState {
+  currentId: number;
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  currentId: 4,
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: state.currentId++,
+        text: action.payload.trim(),
+        state: "todo",
+      });
+    },
+    updateTodo: (state, action) => {
+      const item = state.todos.findIndex((item) => item.id === action.payload);
+      state.todos[item].state =
+        state.todos[item].state === "todo" ? "done" : "todo";
+      state.todos.push(state.todos.splice(item, 1)[0]);
+    },
+    deleteTodo: (state, action) => {
+      const item = state.todos.findIndex((item) => item.id == action.payload);
+      if (item > -1) {
+        state.todos.splice(item, 1);
+      }
+    },
+  },
+});
+
+export default todoSlice.reducer;
+export const { addTodo, updateTodo, deleteTodo } = todoSlice.actions;
