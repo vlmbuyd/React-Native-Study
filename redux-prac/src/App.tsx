6662f3967dabfa7574254893eda22bbe4a318778
Yean <kjan0929@naver.com>
2024-11-21 22:31:20 +0900

    redux thunk를 이용한 API 미들웨어 구현

--- redux-prac/src/App.tsx
+++ redux-prac/src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./reducers";
+import axios from "axios";
+import { Post } from "./reducers/posts";
+import { fetchPosts } from "./actions/posts";
 
 type Props = {
   value: any;
@@ -13,16 +16,23 @@ function App({ value, onIncrement, onDecrement }: Props) {
   const dispatch = useDispatch();
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
 
   const [todoValue, setTodoValue] = useState("");
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
+
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch({ type: "ADD_TODO", text: todoValue });
     setTodoValue("");
   };
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
 
   return (
     <div className="App">
@@ -38,6 +48,11 @@ function App({ value, onIncrement, onDecrement }: Props) {
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit" />
       </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- redux-prac/src/index.tsx
+++ redux-prac/src/index.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-const store = createStore(rootReducer);
+
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store);
+  console.log("action", action);
+  next(action);
+};
+
+const preloadedState = {
+  counter: 0,
+};
+
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+const store = createStore(rootReducer, preloadedState, middleware);
 
 const render = () =>
   root.render(
