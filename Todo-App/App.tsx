d111fe5d63316928d899220f20848628024d1ff9
Yean <kjan0929@naver.com>
2024-11-22 20:00:08 +0900

    setting: react navigation 설치 및 세팅

--- Todo-App/App.tsx
+++ Todo-App/App.tsx
@@ -3,11 +3,29 @@ import { StyleSheet } from "react-native";
 import MainScreen from "./screens/MainScreen";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Main"
+            component={MainScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
