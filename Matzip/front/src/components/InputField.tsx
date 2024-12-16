042679056b4c84ae0a57268973f63c86c2a7721e
Yean <kjan0929@naver.com>
2024-12-16 22:18:51 +0900

    style: Input 공통 컴포넌트 UI 구현

---
+++ Matzip/front/src/components/InputField.tsx
@@ -0,0 +1,81 @@
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInputProps,
+  View,
+} from 'react-native';
+import React, {useRef} from 'react';
+import {TextInput} from 'react-native-gesture-handler';
+import {colors} from '../constants';
+
+interface InputFieldProps extends TextInputProps {
+  disabled?: boolean;
+  error?: string;
+  touched?: boolean;
+}
+
+const deviceHeight = Dimensions.get('screen').height;
+
+export default function InputField({
+  disabled = false,
+  error,
+  touched,
+  ...props
+}: InputFieldProps) {
+  const innerRef = useRef<TextInput | null>(null);
+
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+
+  return (
+    <Pressable onPress={handlePressInput}>
+      <View
+        style={[
+          styles.container,
+          disabled && styles.disabled,
+          touched && Boolean(error) && styles.inputError,
+        ]}>
+        <TextInput
+          ref={innerRef}
+          editable={!disabled}
+          placeholderTextColor={colors.GRAY_500}
+          style={[styles.input, disabled && styles.disabled]}
+          autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
+          {...props}
+        />
+        {touched && Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    padding: deviceHeight > 700 ? 15 : 10,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.BLACK,
+    padding: 0,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  error: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+  },
+});
