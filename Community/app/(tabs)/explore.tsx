f1a5f608e22bcbeea49f84d428d8a18e50e24a4a
김예안 <kjan0929@naver.com>
2025-02-14 22:46:48 +0900

    setting: expo initial commit

---
+++ Community/app/(tabs)/explore.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, View } from "react-native";
+
+export default function ExploreScreen() {
+  return <View></View>;
+}
---
+++ Community/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, View } from "react-native";
+
+export default function HomeScreen() {
+  return <View></View>;
+}
