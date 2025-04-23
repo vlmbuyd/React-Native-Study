a3bc2dcba47774b5d654746e3189dea4594f3d90
김예안 <kjan0929@naver.com>
2025-04-23 23:19:52 +0900

    feat: 버튼 공통 컴포넌트 구현

--- Matzip/src/navigations/stack/AuthStackNavigator.tsx
+++ Matzip/src/navigations/stack/AuthStackNavigator.tsx
@@ -3,8 +3,8 @@ import {StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthHomeScreen from '../../screens/auth/AuthHomeScreen';
 import LoginScreen from '../../screens/auth/LoginScreen';
-import {authNavigations} from '../../constants';
 import SignupScreen from '../../screens/auth/SignupScreen';
+import {authNavigations} from '../../constants';
 
 export type AuthStackParamList = {
   [authNavigations.AUTH_HOME]: undefined;
