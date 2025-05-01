c0536c9c18a71ea69f5cd240e323085e78c96092
김예안 <kjan0929@naver.com>
2025-05-01 16:04:34 +0900

    chore: 서버 디렉토리 생성 및 프론트 디렉토리 별도 분리

--- Matzip/front/src/utils/common.ts
+++ Matzip/front/src/utils/common.ts
@@ -1,11 +1,14 @@
 import {ForwardedRef} from 'react';
 
 function mergeRefs<T>(...refs: ForwardedRef<T>[]) {
+  // ref={fn} 형태로 ref에 콜백이 들어가면 해당 요소의 인스턴스나 DOM 요소를 자동으로 전달함 : node
   return (node: T) => {
     refs.forEach(ref => {
       if (typeof ref === 'function') {
+        // 함수 ref일 경우, 직접 node를 전달
         ref(node);
       } else if (ref) {
+        // 객체 ref일 경우, current 속성에 node를 할당
         ref.current = node;
       }
     });
